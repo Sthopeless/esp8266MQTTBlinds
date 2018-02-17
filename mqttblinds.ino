72aa2f3db46ac0aa113e1c474889cbabe193d297
Sthopeless <hughbrian@hotmail.com>
2018-02-17 15:01:16 +0100

    Update mqttblinds.ino

--- mqttblinds.ino
+++ mqttblinds.ino
@@ -1,5 +1,4 @@
 #include <Servo.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiAP.h>
 #include <ESP8266WiFiGeneric.h>
@@ -11,44 +10,43 @@
 #include <WiFiClientSecure.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
-
 #include <PubSubClient.h>
+/***************************************************************************/
+/****************************** CONFIGURATION ******************************/
+/***************************************************************************/
+#define wifi_ssid "<Your SSID>" //enter your WIFI SSID
+#define wifi_password "<Your Password>" //enter your WIFI Password
 
+#define MQTT_Client "MQTT-Blind"
+#define servo_pin D4
 
-/************ WIFI and MQTT INFORMATION (CHANGE THESE FOR YOUR SETUP) ******************/
-#define wifi_ssid "********" //enter your WIFI SSID
-#define wifi_password "************" //enter your WIFI Password
+#define mqtt_server "<Your IP>" // Enter your MQTT Broker IP
+#define mqtt_user "<Your User id>" //enter your MQTT username
+#define mqtt_password "<Your Password>" //enter your password
+#define mqtt_port 1883
 
-#define mqtt_server "***.***.*.***" // Enter your MQTT server adderss or IP. I use my DuckDNS adddress (yourname.duckdns.org) in this field
-#define mqtt_user "*******" //enter your MQTT username
-#define mqtt_password "***********" //enter your password
+#define mqtt_topic_payload "/" MQTT_Client "/payload/"
+#define mqtt_topic_state "/" MQTT_Client "/state/"
+#define mqtt_topic_brightness "/" MQTT_Client "/brightness/"
 
-
-
-
-
-
-
-
+#define cmnd_on "ON"
+#define cmnd_off "OFF"
+/***************************************************************************/
+/***************** DONT NEED TO CHANGE ANYTHING FROM HERE ******************/
+/***************************************************************************/
 WiFiClient espClient;
 PubSubClient client(espClient);
-
 Servo myservo;
-
 int val;
 int itsatrap = 0;
 
-
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
-
   setup_wifi();
-
-  client.setServer(mqtt_server, 1883); //CHANGE PORT HERE IF NEEDED
+  client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-
 }
-
 
 void setup_wifi() {
 
@@ -56,10 +54,8 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(wifi_ssid);
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -77,29 +73,29 @@ void callback(char* topic, byte* payload, unsigned int length) {
     p[length] = NULL;
     String message(p);
     String mytopic(topic);
-    if (itsatrap == 0 && mytopic == "/blind/bc/command" && message.equals("ON")){  
-      myservo.attach(D3);
+    if (itsatrap == 0 && mytopic == mqtt_topic_payload && message.equals(cmnd_on)){  
+      myservo.attach(servo_pin);
       delay(500);
       myservo.write(90); 
-      client.publish("/blind/bc/state", "ON");
+      client.publish(mqtt_topic_state, cmnd_on);
       delay(1000);
       myservo.detach();
       }
-    else if (mytopic == "/blind/bc/command" && message.equalsIgnoreCase("OFF")){
-      myservo.attach(D3);
+    else if (mytopic == mqtt_topic_payload && message.equalsIgnoreCase(cmnd_off)){
+      myservo.attach(servo_pin);
       delay(500);
       myservo.write(0);  
-      client.publish("/blind/bc/state", "OFF");
+      client.publish(mqtt_topic_state, cmnd_off);
       delay(1000);
       myservo.detach();
     }
-    else if (mytopic == "/blind/bc/level"){
-      myservo.attach(D3);
+    else if (mytopic == mqtt_topic_brightness){
+      myservo.attach(servo_pin);
       delay(500);
       val = message.toInt(); //converts command to integer to be used for positional arrangement
       val = map (val, 0, 99, 0, 180);
       myservo.write(val);
-      client.publish("/blind/bc/state", "ON");
+      client.publish(mqtt_topic_state, cmnd_on);
       delay(3000);
       myservo.detach();
       itsatrap = 1;
@@ -107,31 +103,28 @@ void callback(char* topic, byte* payload, unsigned int length) {
     else{
         itsatrap = 0;
     }
-
 }
 
-
-
 void loop() {
-
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 }
 
-
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = MQTT_Client;
+    clientId += String(random(0xffff), HEX);
     // Attempt to connect
-  if (client.connect("ESPBlindstl666", mqtt_user, mqtt_password)) {
+  if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
       Serial.println("connected");
-
-      client.subscribe("/blind/bc/command");
-      client.subscribe("/blind/bc/level");
-      client.publish("/blind/bc/state", "OFF");
+      client.subscribe(mqtt_topic_payload);
+      client.subscribe(mqtt_topic_brightness);
+      client.publish(mqtt_topic_state, cmnd_off);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -141,28 +134,3 @@ void reconnect() {
     }
   }
 }
-
-/* 
-[In Home Assistant Configuration.yaml]
-light:
-  - platform: mqtt
-    name: "Window Bottom Center"
-    state_topic: "/blind/bc/state"
-    command_topic: "/blind/bc/command"
-    brightness_state_topic: "/blind/bc/state"
-    brightness_command_topic: "/blind/bc/level"
-    brightness_scale: 100
-    qos: 0
-    payload_on: "ON"
-    payload_off: "OFF"
-    optimistic: false
-    retain: true
-    
-    
-    Servo: FS90R from AliExpress
-    
-    Problems: 
-    - ON and OFF rotate the servo the same way
-    - Brightness is not rotating as it should
-    
-    */
